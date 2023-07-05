ddbff73170f8a0978877edbf83536a3e21bac488
rxthew <richardtheumaster@gmail.com>
2023-07-05 22:00:20 +0200

    Establish v1 of user page based on template.

--- src/User.tsx
+++ src/User.tsx
@@ -1,7 +1,128 @@
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+ 
 const User = function User(){
-    return (
-        <div>I am User</div>
-    )
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          username: data.get('username'),
+          password: data.get('password'),
+        });
+      };
+    
+      return (
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Update user details
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                        autoComplete="given-name"
+                        name="firstName"
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="family-name"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            id="username"
+                            label="username"
+                            name="username"
+                            autoComplete="username"
+                            autoFocus
+                         />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            name="current_password"
+                            label="current password"
+                            type="password"
+                            id="current_password"
+                            autoComplete="current-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            name="new_password"
+                            label="new password"
+                            type="password"
+                            id="new_password"
+                            autoComplete="new-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            label="Privilege Code"
+                            name="privilege_code"
+                            type="password"
+                            id="privilege_code"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            label="Admin Code"
+                            name="admin_code"
+                            type="password"
+                            id="admin_code"
+                        />
+                    </Grid>    
+                </Grid>             
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Update your details
+                </Button>
+              </Box>
+            </Box>
+          </Container>
+      );
 };
 
 export default User
