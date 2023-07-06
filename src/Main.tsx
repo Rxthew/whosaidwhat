f83c58515b2f152fe35d9ebc10db4751b642f44f
rxthew <richardtheumaster@gmail.com>
2023-07-06 21:12:41 +0200

    Establish v1 of landing page based on template.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,37 +1,102 @@
-import { useState } from 'react'
+import Badge from '@mui/material/Badge';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Comment from '@mui/icons-material/Comment'
+import CssBaseline  from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+
+interface postPreviewProps {
+  post: {
+    date: string;
+    content: string;
+    title: string;
+  };
+}
 
 
 
-// This is the landing page
-// React router
-//  / landing page
-//  /post/index number  (maybe keep index in useState and increment it.)
-//check what you did with basename
+const PostPreview = function PostPreview(props: postPreviewProps) {
+  const { post } = props;
+
+  const commentsCount = 1 //change to post.comments.length - 1;
+
+  const truncatePostContent = function truncatePostContent(content:string){
+    return content.substring(0,350) + '...'
+  }
+
+  return (
+    <Grid item xs={12} md={6}>
+      <CardActionArea component="a" href="#">
+        <Card sx={{ display: 'flex' }}>
+          <CardContent sx={{ flex: 1 }}>
+            <Typography component="h2" variant="h5">
+              {post.title}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              {post.date}
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {truncatePostContent(post.content)}
+            </Typography>
+            <Grid sx={{display: 'flex'}} justifyContent="space-between">
+              <Typography variant="subtitle1" color="primary">
+                  Continue reading...
+              </Typography>
+              <Badge badgeContent={commentsCount} color="primary">
+                <Comment color="primary" titleAccess="CommentsIcon" fontSize='large' />
+              </Badge>
+            </Grid>    
+          </CardContent>
+        </Card>
+      </CardActionArea>
+    </Grid>
+  );
+}
+
 
 const Main = function(){
-    const [count, setCount] = useState(0)
+
+   //default no posts.
+
+   const posts = [{
+     title: 'This title',
+     content: 'All the content you can think about is here to be entertaining and useful' +
+     ' in equal degree so that you can bla bla bla bla bla bla bla bal here to be entertaining and useful' +
+     ' here to be entertaining and useful  here to be entertaining and useful  here to be entertaining and useful' +
+     ' here to be entertaining and useful here to be entertaining and useful here to be entertaining and useful',
+     date: new Date(Date.now()).toString()
+   },
+
+    {
+      title: 'This second title',
+     content: 'All the secondary content you can think about is here to be entertaining and useful' +
+     ' in equal degree so that you can bla bla bla bla bla bla bla bal here to be entertaining and useful' +
+     ' here to be entertaining and useful  here to be entertaining and useful  here to be entertaining and useful' +
+     ' here to be entertaining and useful here to be entertaining and useful here to be entertaining and useful',
+     date: 'Nov 8 11'
+    }
+  ]
+    
 
     return (
         <>
-        <a href="https://vitejs.dev" target="_blank">
-          vite logo
-        </a>
-        <a href="https://react.dev" target="_blank">
-          react logo
-        </a>
-      <h1>Vie + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      </>
+        <CssBaseline />
+        <Container maxWidth="lg" sx={{py:4}}>
+          <main>
+            <Grid container spacing={4}>
+              {posts.map(
+                function convertToPreview(post){
+                  return <PostPreview key={post.title} post={post} />
+              })}
+            </Grid>
+          </main>
+        </Container>
+        
+        </>
     )
 
 }
