caad5d3aa41bdab00d25e2ad7c33460683acd295
rxthew <richardtheumaster@gmail.com>
2023-08-07 17:12:54 +0200

    Provide Outlet for Blog with appropriate Context.

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -1,21 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
+import { useFetchIndexData } from './helpers/hooks'
+import { IndexInterface } from './helpers/types'
+
 
 
 const Blog = function Blog() {
-  
+  const {user,posts, resetIndexData, setUser, setPosts} = useFetchIndexData()
 
-  //under header should be a react router component. 
-  // :  Landing page "Main.tsx" goes inside here, under Header.
-  // : /signup 
-  // : /login
-  // : /edit profile
-  // error page and mayb sidebars and stuff.
-  //There's also a 'fallback' (hint would be great to avoid state) thing and forms. 
   return (
     <>
-      <Header username="User"/>
-      <Outlet />
+      { user ? (<Header username={user.username}/>) : (<Header />) }
+      <Outlet context={{user, posts, resetIndexData, setUser, setPosts} satisfies IndexInterface}/>
     </>
   )
 }
