8c98f25923ee04f7a8a6320f794567cc32c0aa94
rxthew <richardtheumaster@gmail.com>
2023-08-19 19:49:38 +0200

    Establish context provider to reset loading state.
    Lift Notification & Loading state contexts to Post Context.

--- src/helpers/contexts.tsx
+++ src/helpers/contexts.tsx
@@ -3,10 +3,19 @@ import { notificationReducer } from "./services";
 import { NotificationActionInterface, NotificationReducerInterface } from './types';
 import { produceDefaultNotificationStatus } from "./utils";
 
+export const LoadingStateResetContext = createContext<null | (() => void)>(null);
 export const NotificationsContext = createContext<null | NotificationReducerInterface>(null);
 export const NotificationsDispatchContext = createContext<null | React.Dispatch<NotificationActionInterface>>(null);
 
-export const NotificationsContextProvider = function(children:ReactNode){
+const LoadingStateResetContextProvider = function(children:ReactNode,resetter: () => void){
+    return (
+        <LoadingStateResetContext.Provider value={resetter}>
+            {children}
+        </LoadingStateResetContext.Provider>
+    )
+};
+
+const NotificationsContextProvider = function(children:ReactNode){
         
     const defaultNotificationStatus = produceDefaultNotificationStatus(); 
     const [notifications, dispatch] = useReducer(notificationReducer, defaultNotificationStatus)
@@ -19,3 +28,10 @@ export const NotificationsContextProvider = function(children:ReactNode){
     )
      
 };
+
+export const PostContextProvider = function(children:ReactNode, resetter: () => void){
+    const NotificationContextElement = NotificationsContextProvider(children);
+    return LoadingStateResetContextProvider(NotificationContextElement, resetter)
+
+};
+
