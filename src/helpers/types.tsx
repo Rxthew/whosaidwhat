d8d006d637e2a749ab5ac18e3c08467fa57574b9
rxthew <richardtheumaster@gmail.com>
2023-08-31 17:53:23 +0200

    Apply prettier to src.

--- src/helpers/types.tsx
+++ src/helpers/types.tsx
@@ -1,79 +1,78 @@
-
 export interface CommentInterface {
-    _id: string,
-    content: string,
-    date: string,
-    post: string,
-    user: UserInterface
-    
+  _id: string;
+  content: string;
+  date: string;
+  post: string;
+  user: UserInterface;
 }
 
-export type CommentsType = ReadonlyArray<CommentInterface>
-
+export type CommentsType = ReadonlyArray<CommentInterface>;
 
 export interface FormDialogProps {
-    button: (clickHandler: () => void) => JSX.Element
-    delete: boolean,
-    handleSubmitConstructor: (params: SubmitConstructorParams) => (event: React.FormEvent<HTMLFormElement>) => Promise<void>
-    inputLabel: string,
-    inputText: string,
-    content?: string,
-    submitLabel?: string,
-  
-  }
+  button: (clickHandler: () => void) => JSX.Element;
+  delete: boolean;
+  handleSubmitConstructor: (
+    params: SubmitConstructorParams
+  ) => (event: React.FormEvent<HTMLFormElement>) => Promise<void>;
+  inputLabel: string;
+  inputText: string;
+  content?: string;
+  submitLabel?: string;
+}
 
-type NotificationActionKeys = 'Add Comment' | 'Delete Comment' | 'Edit Comment' | 'Default'
+type NotificationActionKeys =
+  | "Add Comment"
+  | "Delete Comment"
+  | "Edit Comment"
+  | "Default";
 export interface NotificationActionInterface {
-    type: NotificationActionKeys,
+  type: NotificationActionKeys;
 }
 
 interface NotificationStateDetails {
-    message: string
-    status: boolean
+  message: string;
+  status: boolean;
 }
 
 export interface NotificationReducerInterface {
-    'Add Comment Notify': NotificationStateDetails
-    'Delete Comment Notify': NotificationStateDetails   
-    'Edit Comment Notify': NotificationStateDetails
-    
+  "Add Comment Notify": NotificationStateDetails;
+  "Delete Comment Notify": NotificationStateDetails;
+  "Edit Comment Notify": NotificationStateDetails;
 }
 
-
-interface PostInterface { 
-    _id: string,
-    title: string,
-    content: string,
-    date: string,
-    published_status: boolean,
-    user: UserInterface,
-    comments: CommentsType
+interface PostInterface {
+  _id: string;
+  title: string;
+  content: string;
+  date: string;
+  published_status: boolean;
+  user: UserInterface;
+  comments: CommentsType;
 }
 
-
-export type PostsType = ReadonlyArray<PostInterface>
+export type PostsType = ReadonlyArray<PostInterface>;
 
 export interface SubmitConstructorParams {
-    resetIndexData: () => void,
-    resetLoadingState: null | (() => void),
-    setErrors:  React.Dispatch<React.SetStateAction<Record<string, Record<string, string | boolean>>>>
-    setNotifications: React.Dispatch<NotificationActionInterface> | null
-
+  resetIndexData: () => void;
+  resetLoadingState: null | (() => void);
+  setErrors: React.Dispatch<
+    React.SetStateAction<Record<string, Record<string, string | boolean>>>
+  >;
+  setNotifications: React.Dispatch<NotificationActionInterface> | null;
 }
 
-export interface UserInterface { 
-    _id: string,
-    username: string,
-    member_status: "regular" | "privileged" | "admin"
-    first_name?: string,
-    last_name?: string
+export interface UserInterface {
+  _id: string;
+  username: string;
+  member_status: "regular" | "privileged" | "admin";
+  first_name?: string;
+  last_name?: string;
 }
-
 
 export interface IndexInterface {
-    user: UserInterface | null,
-    posts: PostsType | null,
-    resetIndexData: () => void,
-    setUser: React.Dispatch<React.SetStateAction<UserInterface | null>>
-    setPosts: React.Dispatch<React.SetStateAction<PostsType | null>>
+  user: UserInterface | null;
+  posts: PostsType | null;
+  resetIndexData: () => void;
+  setUser: React.Dispatch<React.SetStateAction<UserInterface | null>>;
+  setPosts: React.Dispatch<React.SetStateAction<PostsType | null>>;
 }
