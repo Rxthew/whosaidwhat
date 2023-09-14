d6adfaedab7eff975964f45ee3e118c3b08e91f0
rxthew <richardtheumaster@gmail.com>
2023-09-14 13:44:35 +0200

    Check response validity.

--- src/helpers/hooks.tsx
+++ src/helpers/hooks.tsx
@@ -68,7 +68,7 @@ export const useFetchIndexData = function () {
       }).catch((err: Error) => {
         console.error(err);
       });
-      return response
+      return response && response.ok
         ? setFreshIndexData(
             await response.json().catch((err: Error) => {
               throw err;
